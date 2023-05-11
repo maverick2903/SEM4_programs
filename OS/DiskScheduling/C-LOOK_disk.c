d1080e5c345bd7a32a6f461eec9694ce72a3d401
Varun Viswanath <91796995+maverick2903@users.noreply.github.com>
2023-05-12 00:18:41 +0530

    Create C-LOOK_disk.c

---
+++ OS/DiskScheduling/C-LOOK_disk.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+    int n,req[100],pos,temp;
+    char dir ='u';
+    int tot_movement;
+    printf("Enter number of requests\n");
+    scanf("%d",&n);
+    printf("Enter the requests\n");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&req[i]);
+    }
+    printf("Enter position of head\n");
+    scanf("%d",&pos);
+    //Sorting req in ascending order
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(req[i]>req[j])
+            {
+                temp = req[i];
+                req[i] = req[j];
+                req[j] = temp;
+            }
+        }
+    }
+    //Finding the process less than and closest to head (will be greater than and closest to if direction is down)
+    int min = 9999;
+    int last_index = 0;
+    if(dir=='u')
+    {
+        for(int i=0;i<n;i++)
+        {
+            if(req[i]<pos && abs(req[i]-pos)<=min)
+            {
+                min = abs(req[i]-pos);
+                last_index = i;
+            }
+            else{
+                break;
+            }
+        }
+        tot_movement = 2*req[n-1] - pos + req[last_index] -2*req[0];
+    }
+    printf("Total track movement = %d",tot_movement);
+}

d0d9b8a052815b700f1c9c0daf0f16c5e961dd94
Varun Viswanath <91796995+maverick2903@users.noreply.github.com>
2023-05-18 14:42:51 +0530

    Update InsertionSort_TC.c

--- AoA/InsertionSort_TC.c
+++ AoA/InsertionSort_TC.c
@@ -5,16 +5,37 @@ int main()
 {
     int a[15000];
     clock_t start_t,end_t;
-    double total_t;
+    double avg,best,worst;
+    
+    for(int i=0;i<15000;i++){
+        a[i] = i;
+    }
+    start_t = clock();
+    insertionSort(a);
+    end_t = clock();
+    best = (double)(end_t - start_t);
+    printf("Insertion Sort - Best Case\n");
+    printf("Total time = %f\n",best);
+    
     for(int i=0;i<15000;i++){
         a[i] = rand();
     }
     start_t = clock();
     insertionSort(a);
     end_t = clock();
-    total_t = (double)(end_t - start_t);
+    avg = (double)(end_t - start_t);
     printf("Insertion Sort - Average Case\n");
-    printf("Total time = %f",total_t);
+    printf("Total time = %f\n",avg);
+    
+    for(int i=0;i<15000;i++){
+        a[i] = 14999-i;
+    }
+    start_t = clock();
+    insertionSort(a);
+    end_t = clock();
+    worst = (double)(end_t - start_t);
+    printf("Insertion Sort - Worst Case\n");
+    printf("Total time = %f\n",worst);    
     return 0;
 }
 
